0c6c9938b67ab49d3fce747af981787e7e70af30
sota1235 <sota1235@gmail.com>
2015-05-10 15:17:05 +0900

    modify: ListViewとajaxで映画一覧画面作成(写経)

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,7 @@ var React = require('react-native');
 var {
   AppRegistry,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
@@ -30,7 +31,11 @@ var ReactNative = React.createClass({
       .then((response) => response.json())
       .then((responseData) => {
         this.setState({
-          movies: responseData.movies,
+          dataSource:
+            this.state.dataSource.cloneWithRows(
+              responseData.movies
+              ),
+          loaded: true,
         });
       })
       .done();
@@ -38,7 +43,10 @@ var ReactNative = React.createClass({
 
   getInitialState: function() {
     return {
-      movies: null,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
     };
   },
 
@@ -47,12 +55,17 @@ var ReactNative = React.createClass({
   },
 
   render: function() {
-    if (!this.state.movies) {
+    if (!this.state.loaded) {
       return this.renderLoadingView();
     }
 
-    var movie = this.state.movies[0];
-    return this.renderMovie(movie);
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    );
   },
 
   renderLoadingView: function() {
@@ -113,6 +126,10 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
