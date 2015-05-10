f6dabce4fad6de637f1648fe12cb816e6678442b
sota1235 <sota1235@gmail.com>
2015-05-10 17:29:32 +0900

    fix: いらないデータ消した

--- index.ios.js
+++ index.ios.js
@@ -20,15 +20,6 @@ var {
   View,
 } = React;
 
-var MOCKED_MOVIES_DATA = [
-{
-  title: 'Title',
-  year: '2015',
-  posters: {
-    thumbnail: 'http://i.imgur.com/UePbdph.jpg'
-  }
-}
-]
 var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 var RssReaderNavigator = React.createClass({
